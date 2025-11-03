c7d7fe4a5c22b40241a614a9cc7e1e3246bc6f71
Bruno Steinbruck <brunosteinbruck@MacBook-Air-de-Bruno.local>
2025-11-03 15:11:30 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/ProjectWebService/Course/repositories/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.ProjectWebService.Course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ProjectWebService.Course.entities.Order;
+
+
+public interface OrderRepository extends JpaRepository<Order,Long>{
+	
+	
+}
