55c0b40cfcaa619d3770a9d393efb65f0918ec16
Bruno Steinbruck <brunosteinbruck@MacBook-Air-de-Bruno.local>
2025-11-04 17:38:36 -0300

    correOrderItemRepository type correction

--- src/main/java/com/ProjectWebService/Course/repositories/OrderItemRepository.java
+++ src/main/java/com/ProjectWebService/Course/repositories/OrderItemRepository.java
@@ -3,9 +3,10 @@ package com.ProjectWebService.Course.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ProjectWebService.Course.entities.OrderItem;
+import com.ProjectWebService.Course.entities.pk.OrderItemPK;
 
 
-public interface OrderItemRepository extends JpaRepository<OrderItem,Long> { 
+public interface OrderItemRepository extends JpaRepository<OrderItem,OrderItemPK> { 
 	
 	
 }
